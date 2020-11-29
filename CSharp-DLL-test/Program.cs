6fc2a1901ce1175e7d88c3d1d9a735ae7f486e7b
HongYeseul <ghd8119@naver.com>
2020-11-29 16:47:27 +0900

    C# 동적 라이브러리 사용해보기2 -> Friend Class를 동적라이브러리로 만들기

--- CSharp-DLL-test/Program.cs
+++ CSharp-DLL-test/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using myCalc;
+using myPeopleDLL;
 
 namespace CSharp_DLL_test
 {
@@ -9,8 +11,22 @@ namespace CSharp_DLL_test
         {
             Console.WriteLine("3 + 2 = {0}", Calculator.Add(3, 2));
             Console.WriteLine("3 - 2 = {0}", Calculator.Sub(3, 2));
-            Console.ReadLine();
 
+            List<Friend> listFriends = new List<Friend>();
+            listFriends.Add(new Friend("Onion", "deliciousOnion", 15, false));
+            listFriends.Add(new Friend("Chemi", "Queen_Chemi", 17, false));
+            listFriends.Add(new Friend("mikong", "GoMiko45", 19, false));
+            listFriends.Add(new Friend("Nacho", "Nacho_0101", 31, false));
+            listFriends.Add(new Friend("Onga", "OngChan99", 22, false));
+            listFriends.Add(new Friend("Sara", "OwO_OwO", 21, false));
+            listFriends.Add(new Friend("bunny", "Rabbit12", 28, false));
+            listFriends.Add(new Friend("choco", "ChocoChan", 17, false));
+            listFriends.Add(new Friend("Mitte", "mitteLOVE", 21, false));
+
+            foreach (Friend a in listFriends)
+            {
+                Console.WriteLine(a.Name + " " + a.Id);
+            }
         }
     }
 }
---
+++ myPeopleDLL/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myPeopleDLL
+{
+    public class people
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+        public people() { }
+    }
+
+    public class User : people
+    {
+        public string Password { get; set; }
+
+
+        public User(string name, string id, int age, string pw)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.Age = age;
+            this.Password = pw;
+        }
+        public User() { }
+    }
+
+    public class Friend : people
+    {
+        public bool BestFriend { get; set; }
+
+        public Friend(string name, string id, int age, bool bf)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.Age = age;
+            this.BestFriend = bf;
+        }
+        public Friend() { }
+    }
+}
